9b86955194211ebbee207c867dcf4c5bc885cfc4
ThiagoMachadoL <thiagojkp123@gmail.com>
2022-08-15 21:08:29 -0300

    aula 15 - LABENU

---
+++ script.js
@@ -0,0 +1,49 @@
+//ELEMENTOS NECESSÁRIOS
+/* Inicialização de váriável*/
+/* Condição de continuação*/
+/* Um conjunto de ações*/
+/* Uma alteração*/
+
+// let numero= Number(prompt("Digite um número para somar ou 0 para sair"));
+// let soma=0;
+// while(numero!==0){
+//     soma=soma+numero
+//     console.log(soma);
+//     console.log(numero);
+//     numero = Number(prompt("Digite um número para somar ou 0 para sair"));
+// };
+// console.log("SOMA=",soma);
+
+// //PARTE 2
+// const numero = +prompt("Digite um número ");
+// for(let i=0; i<=numero;i++){
+//     console.log(i)
+// }
+
+//PARTE 3
+// const listaDeNumeros = [2,4,6,8,10,12,14];
+// for (let i=0; i<listaDeNumeros.length;i++) {
+//     console.log(`o numero do indice ${i} é ${listaDeNumeros[i]}`);
+// }
+
+// Atividade 1
+// let usuario = prompt("Digite o seu tipo de usuario, sendo 'A' para Administrador, 'B' para usuario comum e 'C' para usuario assinante").toLowerCase();
+
+// while (usuario !== "a") {
+//     alert("Acesso negado");
+//     usuario = prompt("Digite o seu tipo de usuario, sendo 'A' para Administrador, 'B' para usuario comum e 'C' para usuario assinante").toLowerCase();
+// }
+// alert("Boas vindas");
+// Atividade 2
+
+// let tabuada = 2+2;
+// for (let i=2; i<21;i=i+2) {
+//    console.log(i)
+// }
+
+// Atividade 2
+
+let listaDeStrings = ["carro","moto","lancha"];
+for (let i=0; i<listaDeStrings.length;i++) {
+    console.log(listaDeStrings[i].toUpperCase()); 
+}
